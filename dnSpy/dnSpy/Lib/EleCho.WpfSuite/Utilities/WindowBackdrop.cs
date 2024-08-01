6240f53079a332924881d502c8b5feeb3933dc1d
SlimeNull <slimenull@outlook.com>
2024-08-01 08:38:28 +0800

    Direct reference EleCho.WpfSuite through code

---
+++ dnSpy/dnSpy/Lib/EleCho.WpfSuite/Utilities/WindowBackdrop.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Media;
+
+namespace EleCho.WpfSuite
+{
+    /// <summary>
+    /// Window backdrop
+    /// </summary>
+    public enum WindowBackdrop
+    {
+        /// <summary>
+        /// Automatic
+        /// </summary>
+        Auto = 0,
+
+        /// <summary>
+        /// None
+        /// </summary>
+        None = 1,
+
+        /// <summary>
+        /// Mica
+        /// </summary>
+        Mica = 2,
+
+        /// <summary>
+        /// Acrylic
+        /// </summary>
+        Acrylic = 3,
+
+        /// <summary>
+        /// Mica alternate
+        /// </summary>
+        MicaAlt = 4
+    }
+}
