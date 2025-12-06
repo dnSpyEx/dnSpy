dnSpyEx/dnSpy

commit 4c55fa06803e6eaeef6262c54fbca06613885a6a

Update nullability annotations

dnSpy/dnSpy/Documents/Tabs/DocumentListService.cs
@@ -75,17 +75,20 @@ namespace dnSpy.Documents.Tabs {
 		void SelectList(string name) => selectedIndex = IndexOf(name);
 
 		public void Load(ISettingsSection section) {
-			var listName = section.Attribute<string>(CURRENT_LIST_ATTR);
 			var names = new HashSet<string>(StringComparer.Ordinal);
 			foreach (var listSection in section.SectionsWithName(DOCUMENT_LIST_SECTION)) {
 				var documentList = DocumentList.Create(listSection);
-				if (names.Contains(documentList.Name))
+				if (names.Add(documentList.Name))
 					continue;
 				documentsList.Add(documentList);
 			}
 			hasLoaded = true;
 
-			SelectList(listName);
+			var listName = section.Attribute<string>(CURRENT_LIST_ATTR);
+			if (listName is not null)
+				SelectList(listName);
+			else if (documentsList.Count > 0)
+				SelectList(documentsList[0].Name);
 		}
 		bool hasLoaded;
 

dnSpy/dnSpy/Documents/Tabs/DocumentTabServiceLoader.cs
@@ -53,7 +53,7 @@ namespace dnSpy.Documents.Tabs {
 				foreach (var tgwSection in section.SectionsWithName(TABGROUPWINDOW_SECTION)) {
 					var tgw = SerializedTabGroupWindow.Load(tgwSection);
 					yield return null;
-					if (tgwsHash.Contains(tgw.Name))
+					if (tgwsHash.Add(tgw.Name))
 						continue;
 					tgws.Add(tgw);
 				}

Extensions/dnSpy.Analyzer/TreeNodes/Commands.cs
@@ -293,7 +293,7 @@ namespace dnSpy.Analyzer.TreeNodes {
 		void DeleteNodes() => DeleteNodes(GetNodes());
 		TreeNodeData[]? GetNodes() => GetNodes(analyzerService.Value.TreeView.TopLevelSelection);
 
-		internal static TreeNodeData[]? GetNodes(TreeNodeData[] nodes) {
+		internal static TreeNodeData[]? GetNodes(TreeNodeData[]? nodes) {
 			if (nodes is null)
 				return null;
 			if (nodes.Length == 0 || !nodes.All(a => a.TreeNode.Parent is not null && a.TreeNode.Parent.Parent is null))

Extensions/dnSpy.AsmEditor/Hex/Commands.cs
@@ -74,7 +74,7 @@ namespace dnSpy.AsmEditor.Hex {
 			CreatorObject = creatorObject;
 		}
 
-		public HexContext(IDocumentViewer documentViewer, int? textPosition, object? @ref, bool isDefinition) {
+		public HexContext(IDocumentViewer? documentViewer, int? textPosition, object? @ref, bool isDefinition) {
 			Reference = @ref;
 			IsDefinition = isDefinition;
 			TextPosition = textPosition;

Extensions/dnSpy.AsmEditor/Hex/HexBoxDocumentTabContent.cs
@@ -57,6 +57,9 @@ namespace dnSpy.AsmEditor.Hex {
 				return null;
 
 			var filename = section.Attribute<string>("filename");
+			if (filename is null)
+				return null;
+
 			return hexViewDocumentTabContentCreator.Value.TryCreate(filename);
 		}
 	}

Extensions/dnSpy.Debugger/dnSpy.Debugger/Exceptions/ExceptionListSettings.cs
@@ -82,7 +82,7 @@ namespace dnSpy.Debugger.Exceptions {
 			var exToUpdate = new List<DbgExceptionIdAndSettings>();
 			foreach (var categorySect in section.SectionsWithName("Category")) {
 				var category = categorySect.Attribute<string>("Name");
-				if (string.IsNullOrEmpty(category))
+				if (string2.IsNullOrEmpty(category))
 					continue;
 
 				foreach (var exSect in categorySect.SectionsWithName("Exception")) {
@@ -170,7 +170,7 @@ namespace dnSpy.Debugger.Exceptions {
 					var condSect = condSects[i];
 					var condType = condSect.Attribute<DbgExceptionConditionType?>("Type");
 					var cond = condSect.Attribute<string>("Condition");
-					if (condType is null || string.IsNullOrWhiteSpace(cond) || !IsValid(condType.Value))
+					if (condType is null || string2.IsNullOrWhiteSpace(cond) || !IsValid(condType.Value))
 						return false;
 					conds[i] = new DbgExceptionConditionSettings(condType.Value, cond);
 				}

dnSpy/dnSpy.Contracts.DnSpy/Menus/IMenuItemContext.cs
@@ -62,6 +62,6 @@ namespace dnSpy.Contracts.Menus {
 		/// </summary>
 		/// <typeparam name="T">Type</typeparam>
 		/// <returns></returns>
-		T Find<T>();
+		T? Find<T>();
 	}
 }

dnSpy/dnSpy.Contracts.DnSpy/Settings/ISettingsSection.cs
@@ -46,7 +46,7 @@ namespace dnSpy.Contracts.Settings {
 		/// <typeparam name="T">Type of value</typeparam>
 		/// <param name="name">Name of attribute</param>
 		/// <returns></returns>
-		T Attribute<T>(string name);
+		T? Attribute<T>(string name);
 
 		/// <summary>
 		/// Removes an attribute

commit 31bfa0188febec5313b30881779b8c3e68e9391a

Fix bug introduced in 4c55fa06803e6eaeef6262c54fbca06613885a6a

dnSpy/dnSpy/Documents/Tabs/DocumentListService.cs
@@ -78,7 +78,7 @@ namespace dnSpy.Documents.Tabs {
 			var names = new HashSet<string>(StringComparer.Ordinal);
 			foreach (var listSection in section.SectionsWithName(DOCUMENT_LIST_SECTION)) {
 				var documentList = DocumentList.Create(listSection);
-				if (names.Add(documentList.Name))
+				if (!names.Add(documentList.Name))
 					continue;
 				documentsList.Add(documentList);
 			}

dnSpy/dnSpy/Documents/Tabs/DocumentTabServiceLoader.cs
@@ -53,7 +53,7 @@ namespace dnSpy.Documents.Tabs {
 				foreach (var tgwSection in section.SectionsWithName(TABGROUPWINDOW_SECTION)) {
 					var tgw = SerializedTabGroupWindow.Load(tgwSection);
 					yield return null;
-					if (tgwsHash.Add(tgw.Name))
+					if (!tgwsHash.Add(tgw.Name))
 						continue;
 					tgws.Add(tgw);
 				}